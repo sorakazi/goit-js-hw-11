e6d907f7b20729332b8d0e40b13dddf0c350f625
sorakazi <edmundargana@gmail.com>
2024-05-01 22:16:32 +0800

    update

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,14 @@
+export const BASE_URL = 'https://pixabay.com/api/';
+const API_KEY = '43667057-3a723c15f5345c10cebb45902';
+
+export const options = {
+  params: {
+    key: API_KEY,
+    q: '',
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: true,
+    page: 1,
+    per_page: 40,
+  },
+};
